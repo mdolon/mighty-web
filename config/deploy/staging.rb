0ee421593ad4c1bc1d01e9bb4d5eec33c61642e4
Monji Dolon <mdolon@gmail.com>
2014-11-05 16:42:09 -0500

    Basic cap setup

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{deploybot@mightyapp.co}
+role :web, %w{deploybot@mightyapp.co}
+role :db,  %w{deploybot@mightyapp.co}
 
 
 # Extended Server Syntax
@@ -15,7 +15,7 @@ role :db,  %w{deploy@example.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server 'staging.mightyapp.co', user: 'deploybot', roles: %w{web app}, my_property: :my_value
 
 
 # Custom SSH Options
