7935402775c83b0b82dab0aee87e9b4a1c815fb7
Monji Dolon <mdolon@gmail.com>
2014-11-08 23:52:49 -0500

    Update sendgrid code

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'monji',
+  :user_name => 'mightyapp',
   :password => 'm1ghtju$td3pl0y',
   :domain => 'mightyapp.co',
   :address => 'smtp.sendgrid.net',
